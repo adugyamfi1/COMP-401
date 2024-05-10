9407abd780042bad9223ab424a8117e62a0486de
adugyamfi1 <124085060+adugyamfi1@users.noreply.github.com>
2024-05-10 08:56:45 -0400

    Create data.ts

---
+++ Signup/src/services/shop/data.ts
@@ -0,0 +1,137 @@
+import { IOrderStatus } from "./types";
+
+export const orderStatuses: IOrderStatus[] = [
+  "PENDING",
+  "PROCESSING",
+  "SHIPPED",
+  "DELIVERED",
+  "CANCELLED",
+  "REFUNDED",
+  "ON HOLD",
+  "BACKORDERED",
+  "RETURNED",
+  "FAILED",
+  "COMPLETED",
+];
+
+// export const shopMain: string[] = [
+//   "Fashion and Apparel",
+//   "Electronics",
+//   "Home and Furniture",
+//   "Beauty and Personal Care",
+//   "Toys and Games",
+//   "Books and Stationery",
+//   "Sports and Outdoors",
+//   "Health and Wellness",
+//   "Automotive",
+//   "Jewelry and Watches",
+//   "Food and Grocery",
+//   "Art and Craft Supplies",
+//   "Pet Supplies",
+//   "Travel and Luggage",
+//   "Specialty Shops",
+// ];
+
+// const shopCategories = [
+//   {
+//     category: "Fashion and Apparel",
+//     subcategories: [
+//       "Men's Clothing",
+//       "Women's Clothing",
+//       "Kids' Clothing",
+//       "Shoes and Accessories",
+//     ],
+//   },
+//   {
+//     category: "Electronics",
+//     subcategories: [
+//       "Smartphones and Accessories",
+//       "Laptops and Computers",
+//       "Audio and Headphones",
+//       "Cameras and Photography",
+//       "Computers Accessories",
+//     ],
+//   },
+//   {
+//     category: "Home and Furniture",
+//     subcategories: [
+//       "Furniture and Decor",
+//       "Bedding and Linens",
+//       "Kitchen and Dining",
+//       "Home Improvement",
+//     ],
+//   },
+//   {
+//     category: "Beauty and Personal Care",
+//     subcategories: [
+//       "Skincare",
+//       "Haircare",
+//       "Makeup and Cosmetics",
+//       "Fragrances",
+//     ],
+//   },
+//   {
+//     category: "Toys and Games",
+//     subcategories: ["Toys for Kids", "Board Games and Puzzles", "Outdoor Play"],
+//   },
+//   {
+//     category: "Books and Stationery",
+//     subcategories: [
+//       "Fiction and Non-Fiction Books",
+//       "Office Supplies",
+//       "Notebooks and Journals",
+//     ],
+//   },
+//   {
+//     category: "Sports and Outdoors",
+//     subcategories: ["Sports Equipment", "Outdoor Gear", "Activewear"],
+//   },
+//   {
+//     category: "Health and Wellness",
+//     subcategories: [
+//       "Vitamins and Supplements",
+//       "Fitness Equipment",
+//       "Personal Care Products",
+//     ],
+//   },
+//   {
+//     category: "Automotive",
+//     subcategories: [
+//       "Car Parts and Accessories",
+//       "Tools and Equipment",
+//       "Maintenance Products",
+//     ],
+//   },
+//   {
+//     category: "Jewelry and Watches",
+//     subcategories: ["Fine Jewelry", "Fashion Jewelry", "Watches"],
+//   },
+//   {
+//     category: "Food and Grocery",
+//     subcategories: ["Fresh Produce", "Pantry Staples", "Snacks and Beverages"],
+//   },
+//   {
+//     category: "Art and Craft Supplies",
+//     subcategories: ["Painting and Drawing Supplies", "Craft Kits", "DIY Tools"],
+//   },
+//   {
+//     category: "Pet Supplies",
+//     subcategories: ["Pet Food", "Toys and Accessories", "Grooming Products"],
+//   },
+//   {
+//     category: "Travel and Luggage",
+//     subcategories: [
+//       "Luggage and Suitcases",
+//       "Travel Accessories",
+//       "Outdoor Gear",
+//     ],
+//   },
+//   {
+//     category: "Specialty Shops",
+//     subcategories: [
+//       "Eco-Friendly Products",
+//       "Handmade and Artisan Goods",
+//       "Customized and Personalized Items",
+//     ],
+//   },
+// ];
